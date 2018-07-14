1da7a710f221cf0bf7fff93162a245ddee357696
Susan Meyers <smmeyers24@gmail.com>
2018-07-14 01:24:44 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,15 +1,18 @@
 class Dog
   def bark
     puts "Woof!"
+  end
+end
 
     fido = Dog.new
     fido.bark
+  end
 
     def sit
       puts "The Dog is sitting"
+    end
 
       fido = Dog.new
       fido.sit
-  end
   end
 end
